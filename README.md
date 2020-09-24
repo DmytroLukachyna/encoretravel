b6be5855a923cdae45c81470dec06d99679f4fc9
Dmytro Lukachyna <guidecco@ukr.net>
2020-09-24 13:51:51 +0300

    v0.0.1, First verified commit

---
+++ README.md
@@ -0,0 +1,1 @@
+encoretravel.com.ua
---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader-wrapper").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader-wrapper").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+        document.querySelector('#wrapper').style.opacity = "1";
+    }
+};
